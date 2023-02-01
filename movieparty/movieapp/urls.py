78da41d546be04ecd01ef69ed6633d391e02d207
Vladyslav <sharpilo.vladislav96@gmail.com>
2023-02-01 22:16:36 +0200

    Добавил карусель и проработал формы обновления, удаления для комнат и фильмов

--- movieparty/movieapp/urls.py
+++ movieparty/movieapp/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('', views.base_views, name='base_views'),
     path('movie-create/', views.movie_create, name='movie_create'),
     path('movies-list/', views.movies_list, name='movies_list'),
+    path('movie-delete/<int:item_id>/', views.movie_delete, name='movie_delete'),
+    path('movie-update/<int:item_id>/', views.movie_update, name='movie_update'),
     path('room-create/', views.room_create, name='room_create'),
+    path('rooms-list/', views.rooms_list, name='rooms_list'),
+    path('room-delete/<int:item_id>/', views.room_delete, name='room_delete'),
+    path('room-update/<int:item_id>/', views.room_update, name='room_update'),
 ]
--- movieparty/movieapp/views.py
+++ movieparty/movieapp/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
+import os
 
 from .forms import MovieForm, RoomForm
-from .models import Movie
+from .models import Movie, Room
 
 
 def room_create(request):
@@ -11,10 +12,31 @@ def room_create(request):
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('rooms_list')
+        return HttpResponseRedirect(reverse_lazy('rooms_list'))
     else:
         form = RoomForm()
         return render(request, 'movieapp/room_create.html', {'form': form})
+
+
+def room_delete(request, item_id):
+    item = Room.objects.get(pk=item_id)
+    item.delete()
+    return HttpResponseRedirect(reverse_lazy('rooms_list'))
+
+
+def room_update(request, item_id):
+    instance = get_object_or_404(Room, id=item_id)
+    form = RoomForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse_lazy('rooms_list'))
+    return render(request, 'movieapp/room_update.html', {'form': form})
+
+
+def rooms_list(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'movieapp/rooms_list.html', context=context)
 
 
 def movie_create(request):
@@ -33,6 +55,37 @@ def movie_create(request):
     return render(request, 'movieapp/movie_create.html', {'form': form})
 
 
+def movie_delete(request, item_id):
+    item = Movie.objects.get(pk=item_id)
+    if item.video:
+        if os.path.exists(item.video.path):
+            os.remove(item.video.path)
+    if item.cover_image:
+        if os.path.exists(item.cover_image.path):
+            os.remove(item.cover_image.path)
+    item.delete()
+    return HttpResponseRedirect(reverse_lazy('movies_list'))
+
+
+def movie_update(request, item_id):
+    instance = get_object_or_404(Movie, id=item_id)
+    if request.method == 'POST' or request.method == 'FILES':
+        form = MovieForm(request.POST or request.FILES, instance=instance)
+        if form.is_valid():
+            if instance.video:
+                if os.path.exists(instance.video.path):
+                    os.remove(instance.video.path)
+            if instance.cover_image:
+                if os.path.exists(instance.cover_image.path):
+                    os.remove(instance.cover_image.path)
+            item = form.save(commit=False)
+            item.save()
+            return HttpResponseRedirect(reverse_lazy('movies_list'))
+    else:
+        form = MovieForm(instance=instance)
+    return render(request, 'movieapp/movie_update.html', {'form': form})
+
+
 def movies_list(request):
     movies = Movie.objects.all()
     context = {'movies': movies}
@@ -40,4 +93,6 @@ def movies_list(request):
 
 
 def base_views(request):
-    return render(request, 'movieapp/base.html')
+    movies = Movie.objects.all()
+    context = {'movies': movies}
+    return render(request, 'movieapp/base.html', context=context)
